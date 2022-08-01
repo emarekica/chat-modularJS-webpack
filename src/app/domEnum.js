d6f594200b21f32e2d09d4665d0675768aebc10d
emarekica <emarekica@gmail.com>
2022-08-01 20:43:54 +0200

    rearrange folders

--- src/app/domEnum.js
+++
@@ -1,8 +0,0 @@
-const DOM = {
-  MEMBERS: document.querySelector(".members"),
-  MESSAGE: document.querySelector(".messages"),
-  INPUT: document.querySelector(".messageFormInput"),
-  FORM: document.querySelector(".messageForm"),
-};
-
-export default DOM;
--- src/helpers/randomColor.js
+++
@@ -1,5 +0,0 @@
-// exported to connection.js
-
-export function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
-}
